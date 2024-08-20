565e5cba031b5f939b56d8bf544f8f488c9e1596
Pawel <pawel3ala@gmail.com>
2024-08-20 14:06:22 +0200

    screens & navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -33,7 +33,8 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="details" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
     </ThemeProvider>
